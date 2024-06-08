65454881c0d1432035e0f8d56593abcc9d1db613
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-06-08 07:41:20 -0700

    create
    add whole page

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+export interface Product {
+  id: number;
+  slug: string;
+  name: string;
+  image: CategoryImage;
+  category: string;
+  categoryImage: CategoryImage;
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: Include[];
+  gallery: Gallery;
+  others: Other[];
+}
+
+export interface CategoryImage {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export interface Gallery {
+  first: CategoryImage;
+  second: CategoryImage;
+  third: CategoryImage;
+}
+
+export interface Include {
+  quantity: number;
+  item: string;
+}
+
+export interface Other {
+  slug: string;
+  name: string;
+  image: CategoryImage;
+}
+
+type Data = Product[];
+
+function App() {
+  const [products, setProducts] = useState<null | Data>(null);
+
+  const getData = async () => {
+    const res = await fetch("http://localhost:5173/data.json");
+    const data = await res.json();
+    setProducts(data);
+  };
+  return (
+    <>
+      <button onClick={() => getData()}>Get Data</button>
+
+      {products?.map((product) => {
+        return (
+          <div key={product.id}>
+            <img
+              src={`http://localhost:5173/${product.image.desktop}`}
+              alt=""
+            />
+            <p>{product.name}</p>
+            <p>{product.id}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
